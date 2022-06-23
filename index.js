2dc3affeaf5eb1b25d6eddf1da7c4ccb22c24a93
Wanda Arriola <wanda.longo@hotmail.com>
2022-06-22 21:49:07 -0300

    primer intento de la entrega 1

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 
-let monto= parseInt(prompt("ingrese el monto de dinero que quiere invertir"));
+let patente = prompt("Este programa indica el saldo de la multa. Ingrese su patente (ESC para salir)")
+function solicitarPatente() {
 
-let retiro= parseInt (prompt("ingrese en que mes quire retirar el deinero"))
+    return patente;
+}
+solicitarPatente()
 
-for (let i=1; i<= retiro; i++){
-    let resultado = monto*i
-    if (i===retiro+1){
-        break
-    } else{
 
-    console.log ("en el mes " + i + " usted va a retirar "+ resultado)
+let velocidad = parseInt(prompt("ingrese a la velocidad que circulaba"))
+function solicitarVelocidad() {
+
+    return velocidad
+}
+solicitarVelocidad()
+
+
+function calculoMulta() {
+    let patente2 = solicitarPatente()
+    while (patente2 !== "ESC") {
+        if (patente2 !== " ") {
+            solicitarVelocidad()
+            if (velocidad <= 60) {
+                alert("la patente: " + patente + " no debe multas")
+                break
+            } else {
+                alert ( "la patente " + patente + " debe pagar una multa de $60.000")
+                break
+            }
+        }else {
+            alert("ingrse su patente")
+        }
     }
 }
+
+calculoMulta()
+
+
+
