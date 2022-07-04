edff630d24db035d27fb63a5ffbfbde773108b9c
Wanda Arriola <wanda.longo@hotmail.com>
2022-07-04 20:19:43 -0300

    función de orden superior

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
 
-function solicitarPatente() {
+/*function solicitarPatente() {
     let patente = prompt("Este programa indica el saldo de la multa según la velocidad. Ingrese su patente (ESC para salir)")
     return patente;
 }
 
 
 function solicitarVelocidad() {
-    let velocidad = parseInt(prompt("ingrese si uds circulaba a: \n menos de 50kmh \n mas de 60kmh \n mas de 80kmh \n mas de 100kmh"))
+    let velocidad = parseInt(prompt("ingrese la velocidad a la que circulaba"))
     return velocidad
 }
 
@@ -18,19 +18,19 @@ function calculoMulta() {
     while (patente2 !== "ESC"){
         if (patente2 !== ""){
             
-            velocidad2 = solicitarVelocidad ()
+            let velocidad2 = solicitarVelocidad ()
                 
             if (velocidad2 < 60){
                     alert ("La patente: " + patente2 + " no debera pagar multa")
                     break
-                } else if (velocidad2 == 60){
-                    alert ("La patente: " + patente2 + " debera pagar una multa de $60.000")
+                } else if (velocidad2 > 100){
+                    alert ("La patente: " + patente2 + " debera pagar una multa de $100.000")
                     break
-                } else if (velocidad2 == 80){
+                } else if (velocidad2 > 80){
                     alert ("La patente: " + patente2 + " debera pagar una multa de $80.000")
                     break
-                } else if (velocidad2 == 100){
-                    alert ("La patente: " + patente2 + " debera pagar una multa de $100.000")
+                } else if (velocidad2 > 60){
+                    alert ("La patente: " + patente2 + " debera pagar una multa de $60.000")
                     break
                 }
 
@@ -42,10 +42,66 @@ function calculoMulta() {
     }
 }
 
+calculoMulta()*/
 
 
+// busqueda de zona //
 
-calculoMulta()
+const zona = [
+
+    { districto: "Vince Lopez", direccion: "Savedra 1256" },
+    { districto: "San Isidro", direccion: "Peron 5896" },
+    { districto: "San Fernando", direccion: "Pirovano 8956" },
+    { districto: "Tigre", direccion: "Quiroz 2878" },
+    { districto: "Escobar", direccion: "Sarmiento 7856" },
+    { districto: "Pilar", direccion: "Moreno 5896" },
+    { districto: "José C. Paz", direccion: "Liniers 3256" },
+    { districto: "Malvinas Argentinas", direccion: "Pasos 1256" },
+    { districto: "San miguel", direccion: "Victoria 586" },
+    { districto: "San Martín", direccion: "Ilia 7926" },
+]
 
 
+alert("Este programa de verificación de multas corresponde a zona norte")
 
+function solicitarZona() {
+    let zona = prompt("Indique el districto donde fue cometida la infracción")
+    return zona;
+}
+
+let zona2 = solicitarZona()
+
+const buscando = zona.find((nombre) => nombre.districto === zona2)
+
+console.log (buscando)
+
+// fin busqueda //
+
+//solicitud de turno para dia y horario//
+
+function solicitarDia() {
+    let dia = prompt("ingrese un día de la semana de lunes a viernes")
+    return dia
+}
+
+function solicitarHora() {
+    let hora = parseInt(prompt("los horarios disponibles son: \n 10:30 \n 11:45 \n 13:15 \n 15:00"))
+    return hora
+}
+
+function turno() {
+
+    dia2 = solicitarDia()
+    hora2 = solicitarHora()
+
+    const listaDias = ["lunes", "martes", "miércoles", "jueves", "viernes"];
+
+    console.log(listaDias.includes(dia2))
+    
+    alert("usted tiene turno el día " + dia2 + " a las " + hora2 + " en " + zona2 )
+
+}
+
+turno ()
+
+//fin solicitud de dia y horario //
